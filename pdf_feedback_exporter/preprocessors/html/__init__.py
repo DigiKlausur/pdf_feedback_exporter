9063ad9837c6d48e07271d1ed5ec0b4e8b5135e7
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-12-06 17:55:19 +0100

    Add preprocessors to __init__

--- pdf_feedback_exporter/preprocessors/html/__init__.py
+++ pdf_feedback_exporter/preprocessors/html/__init__.py
@@ -1,10 +1,14 @@
-from .changescorecolor import ChangeScoreColor
-from .fixmathlinebreaks import FixMathLineBreaks
-from .makecolorsimportant import MakeColorsImportant
+from .embedimages import EmbedImages
 from .prettifytables import PrettifyTables
+from .changescorecolor import ChangeScoreColor
+from .changetotalscore import ChangeTotalScore
+from .makecolorsimportant import MakeColorsImportant
+from .fixmathlinebreaks import FixMathLineBreaks
 
 __all__ = [
     "ChangeScoreColor",
+    "ChangeTotalScore",
+    "EmbedImages",
     "FixMathLineBreaks",
     "MakeColorsImportant",
     "PrettifyTables",
